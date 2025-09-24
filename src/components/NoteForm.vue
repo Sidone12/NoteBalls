23eef13c6c1f6ef2f1fb5dd0d9e411c3fd81b1b7
Oleksii <sidone666@gmail.com>
2025-09-24 22:14:43 +0300

    feat: enhance NoteForm with v-model and validation for adding notes

--- src/components/NoteForm.vue
+++ src/components/NoteForm.vue
@@ -2,20 +2,52 @@
     <div class="card has-background-success-dark p-4 mb-4">
         <div class="field ">
             <div class="control">
-                <textarea class="textarea" placeholder="Add a new note"></textarea>
+                <textarea ref="noteInput" class="textarea" placeholder="Add a new note" v-model="newNote"></textarea>
             </div>
         </div>
 
         <div class="field is-grouped is-grouped-right">
             <div class="control">
-                <button class="button is-link has-background-success">Add New Note</button>
+                <button :disabled="!isNoteValid" @click="addNote" class="button is-link has-background-success">Add New
+                    Note</button>
             </div>
         </div>
     </div>
 </template>
 
 <script lang="ts" setup>
+import { ref, computed } from 'vue';
+import { NoteType } from '@/types';
 
+const emit = defineEmits<{
+    (e: 'add-note', note: NoteType): void;
+}>();
+
+
+const newNote = ref('');
+const noteInput = ref<HTMLTextAreaElement | null>(null);
+const focusInput = () => {
+    noteInput.value?.focus();
+};
+
+
+const isNoteValid = computed(() => {
+    return newNote.value.trim() !== '';
+});
+
+const addNote = () => {
+    if (!isNoteValid.value) return;
+
+    const note: NoteType = {
+        id: Date.now(),
+        content: newNote.value.trim(),
+    };
+
+    emit('add-note', note);
+    newNote.value = '';
+};
+
+defineExpose({ focusInput });
 </script>
 
 <style>
