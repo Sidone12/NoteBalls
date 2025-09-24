e0afc50b40a5e175a055f922b70b4a27fb310fa8
Oleksii <sidone666@gmail.com>
2025-09-24 16:42:09 +0300

    add NoteForm component for creating new notes

---
+++ src/components/NoteForm.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="card has-background-success-dark p-4 mb-4">
+        <div class="field ">
+            <div class="control">
+                <textarea class="textarea" placeholder="Add a new note"></textarea>
+            </div>
+        </div>
+
+        <div class="field is-grouped is-grouped-right">
+            <div class="control">
+                <button class="button is-link has-background-success">Add New Note</button>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style>
+
+</style>
