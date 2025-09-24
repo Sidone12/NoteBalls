441e296736440c4bbf19c4d8c5a996f341c1b0ab
Oleksii <sidone666@gmail.com>
2025-09-24 16:42:30 +0300

    add ViewNotes component with NoteForm and card layout for displaying notes

--- src/views/ViewNotes.vue
+++ src/views/ViewNotes.vue
@@ -1,10 +1,25 @@
 <template>
-  <div>
-    Notes
-  </div>
+    <div class="notes">
+        <NoteForm />
+
+        <div class="card mb-4" v-for="i in 4">
+            <div class="card-content">
+                <div class="content">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec
+                    iaculis mauris.
+                </div>
+            </div>
+            <footer class="card-footer">
+                <a href="#" class="card-footer-item">Edit</a>
+                <a href="#" class="card-footer-item">Delete</a>
+            </footer>
+        </div>
+    </div>
 </template>
 
 <script lang="ts" setup>
+import NoteForm from '@/components/NoteForm.vue';
+
 </script>
 
 
