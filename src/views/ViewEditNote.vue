b3b2ffe7dd021eab87c4b6949f7ab122ed59a858
Oleksii <sidone666@gmail.com>
2025-09-30 00:24:12 +0300

    add ViewEditNote component and update ViewNotes to use AddEditNote for note creation

---
+++ src/views/ViewEditNote.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    id is : {{ $route.params.id }}
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style>
+
+</style>
--- src/views/ViewNotes.vue
+++ src/views/ViewNotes.vue
@@ -1,14 +1,35 @@
 <template>
-    <div class="notes">
-        <NoteForm />
-        <Note v-for="note in storeNotes.notes" :key="note.id" :note="note" />
-    </div>
+  <div class="notes">
+    <AddEditNote v-model="newNote" ref="addEditNoteRef" placeholder="Add a new note">
+      <template #buttons>
+        <button :disabled="!newNote.trim()" @click="addNote" class="button is-link has-background-success">
+          Add New Note
+        </button>
+      </template>
+    </AddEditNote>
+    <Note v-for="note in storeNotes.notes" :key="note.id" :note="note" />
+  </div>
 </template>
 
 <script lang="ts" setup>
-import NoteForm from '@/components/NoteForm.vue';
+import { ref } from 'vue';
+import AddEditNote from '@/components/AddEditNote.vue';
 import Note from '@/components/Note.vue';
+import type { NoteType } from '@/types';
 import { useStoreNotes } from '@/stores/storeNotes';
 
 const storeNotes = useStoreNotes();
+
+const newNote = ref('');
+const addEditNoteRef = ref<InstanceType<typeof AddEditNote> | null>(null);
+
+const addNote = () => {
+  const note: NoteType = {
+    id: Date.now(),
+    content: newNote.value
+  };
+  storeNotes.addNote(note);
+  newNote.value = '';
+  addEditNoteRef.value?.focusTextarea();
+};
 </script>
