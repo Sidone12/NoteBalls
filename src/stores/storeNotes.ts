d0250b1f8b66e3ccfaf9db8f89f1801944dc14b8
Oleksii <sidone666@gmail.com>
2025-10-01 16:44:02 +0300

    update storeNotes add new functionality for editing notes and computing quantity of notes and characters

--- src/stores/storeNotes.ts
+++ src/stores/storeNotes.ts
@@ -1,5 +1,5 @@
 import {defineStore} from 'pinia';
-import {ref} from 'vue';
+import {ref, computed} from 'vue';
 import type {NoteType} from '@/types/index';
 
 export const useStoreNotes = defineStore('notes', () => {
@@ -14,17 +14,45 @@ export const useStoreNotes = defineStore('notes', () => {
       content:
         'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus neciaculis mauris.',
     },
+    {
+      id: 3,
+      content: 'some text',
+    },
   ]);
+  // Getters
+  const totalNotesCount = computed(() => notes.value.length);
 
+  const totalCharactersCount = computed(() =>
+    notes.value.reduce((acc, note) => acc + note.content.length, 0)
+  );
+  const getNoteById = (id: number) => {
+    return notes.value.find(note => note.id === id)?.content || '';
+  };
+  // Actions  
   const addNote = (note: NoteType) => {
     notes.value.unshift(note);
   };
 
-  const removeNote = (noteId:number) => {
-      notes.value = notes.value.filter((n: NoteType) => {
-          return n.id !== noteId;
+  const removeNote = (noteId: number) => {
+    notes.value = notes.value.filter((n: NoteType) => {
+      return n.id !== noteId;
     });
   };
+  
+  const updateNote = (id: number, newContent: string) => {
+    let note = notes.value.find(note => note.id === id);
+    if (note) {
+      note.content = newContent;
+    }
+  };
 
-  return {notes, addNote, removeNote};
+  return {
+    notes,
+    addNote,
+    removeNote,
+    getNoteById,
+    updateNote,
+    totalNotesCount,
+    totalCharactersCount,
+  };
 });
