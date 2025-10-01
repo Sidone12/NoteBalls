c29b129898fc90cfccfc5787f38c6b68b69fc941
Oleksii <sidone666@gmail.com>
2025-10-01 16:47:39 +0300

    refactore: upd ViewEditNote for updating notes and its functionality

--- src/views/ViewEditNote.vue
+++ src/views/ViewEditNote.vue
@@ -1,13 +1,35 @@
 <template>
-  <div>
-    id is : {{ $route.params.id }}
-  </div>
+  <AddEditNote label="Edit note" bgColor="grey" v-model="noteContent" ref="addEditNoteRef"
+    placeholder="Edit a new note">
+    <template #buttons>
+      <button @click="router.back()" class="button is-link is-light mr-2">
+        Cancel
+      </button>
+      <button :disabled="!noteContent.trim()" @click="updateNote" class="button is-link has-background-success">
+        Save Changes
+      </button>
+    </template>
+  </AddEditNote>
 </template>
 
 <script lang="ts" setup>
+import { ref } from 'vue';
+import AddEditNote from '@/components/AddEditNote.vue';
+import { useStoreNotes } from '@/stores/storeNotes';
+import { useRoute, useRouter } from 'vue-router';
+
+const store = useStoreNotes();
+const route = useRoute();
+const router = useRouter();
+
+const noteContent = ref('');
+
+noteContent.value = store.getNoteById(Number(route.params.id));
+
+const updateNote = () => {
+  if (!noteContent.value.trim()) return;
+  store.updateNote(Number(route.params.id), noteContent.value.trim());
+  router.back();
+};
 
 </script>
-
-<style>
-
-</style>
