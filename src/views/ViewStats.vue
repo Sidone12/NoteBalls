25d9b5ae568325cd9ac734e612e0d840b52ae2ec
Oleksii <sidone666@gmail.com>
2025-10-01 16:49:45 +0300

    refactore: upd ViewStats gathering all info from store

--- src/views/ViewStats.vue
+++ src/views/ViewStats.vue
@@ -1,13 +1,27 @@
 <template>
-  <div>
-    Stats
+  <div class="stats">
+    <table class="table is-fullwidth">
+      <thead>
+        <tr>
+          <th>Stats</th>
+          <th>Value</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>Number of Notes</td>
+          <td>{{ store.totalNotesCount }}</td>
+        </tr>
+        <tr>
+          <td>Number of Characters (of all notes)</td>
+          <td>{{ store.totalCharactersCount }}</td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
 <script lang="ts" setup>
-
+import { useStoreNotes } from '@/stores/storeNotes';
+const store = useStoreNotes();
 </script>
-
-<style>
-
-</style>
