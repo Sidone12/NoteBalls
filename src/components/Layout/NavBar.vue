8f3d4471b01cda81fa089abfe639c7e2621f1b3c
Oleksii <sidone666@gmail.com>
2025-10-16 00:14:23 +0300

    refactor: clean up NavBar.vue template and add logout functionality

--- src/components/Layout/NavBar.vue
+++ src/components/Layout/NavBar.vue
@@ -1,59 +1,67 @@
 <template>
-    <nav class="navbar is-success" role="navigation" aria-label="main navigation">
-        <div class="container is-max-desktop px-2">
-            <div class="navbar-brand">
-                <div class="navbar-item is-size-4 has-text-weight-bold is-family-monospace">
-                    NoteBalls
-                </div>
+<nav class="navbar is-success" role="navigation" aria-label="main navigation">
+    <div class="container is-max-desktop px-2">
+        <div class="navbar-brand">
+            <div class="navbar-item is-size-4 has-text-weight-bold is-family-monospace">
+                NoteBalls
+            </div>
 
-                <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false"
-                    :class="{ 'is-active': showMobileNav }" @click.prevent="showMobileNav = !showMobileNav"
-                    data-target="navbarBasicExample" ref="navbarBurger">
+            <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" :class="{ 'is-active': showMobileNav }" @click.prevent="showMobileNav = !showMobileNav" data-target="navbarBasicExample" ref="navbarBurger">
                     <span aria-hidden="true"></span>
                     <span aria-hidden="true"></span>
                     <span aria-hidden="true"></span>
                     <span aria-hidden="true"></span>
                 </a>
-            </div>
+        </div>
 
-            <div ref="target" id="navbarBasicExample" class="navbar-menu" :class="{ 'is-active': showMobileNav }">
-                <div class="navbar-end">
-                    <div class="buttons">
-                        <RouterLink @click="showMobileNav = false" to="/" class="navbar-item" active-class="is-active">
-                            Notes
-                        </RouterLink>
-                        <RouterLink @click="showMobileNav = false" to="/stats" class="navbar-item"
-                            active-class="is-active">
-                            Stats
-                        </RouterLink>
-                    </div>
+        <div ref="target" id="navbarBasicExample" class="navbar-menu" :class="{ 'is-active': showMobileNav }">
+            <div class="navbar-start">
+                <button v-if="storeAuth.signedUser.id" @click="logout" class="button is-small is-info  m-3">
+                    Logout {{ storeAuth.signedUser.email }}</button>
+            </div>
+            <div class="navbar-end">
+                <div class="buttons">
+                    <RouterLink @click="showMobileNav = false" to="/" class="navbar-item" active-class="is-active">
+                        Notes
+                    </RouterLink>
+                    <RouterLink @click="showMobileNav = false" to="/stats" class="navbar-item" active-class="is-active">
+                        Stats
+                    </RouterLink>
                 </div>
             </div>
         </div>
-    </nav>
+    </div>
+</nav>
 </template>
 
 <script lang="ts" setup>
 import { RouterLink } from 'vue-router';
-import { ref } from 'vue';
+import {  ref } from 'vue';
 import { onClickOutside } from '@vueuse/core'
 import { useTemplateRef } from 'vue'
+import { useStoreAuth } from '@/stores/storeAuth'; 
 
-const target = useTemplateRef<HTMLElement>('target')
-const navbarBurger = useTemplateRef<HTMLElement>('navbarBurger')
+const storeAuth = useStoreAuth()
 
-onClickOutside(target, () => showMobileNav.value = false , {
+const target = useTemplateRef < HTMLElement > ('target')
+const navbarBurger = useTemplateRef < HTMLElement > ('navbarBurger')
+
+onClickOutside(target, () => showMobileNav.value = false, {
     ignore: [navbarBurger]
 })
 
 const showMobileNav = ref(false)
+const logout = () => {
+    showMobileNav.value = false;
+    storeAuth.logoutUser()
+}
 </script>
 
 <style>
-@media  (max-width: 1023px) {
+@media (max-width: 1023px) {
     .navbar-menu {
         position: absolute;
-        left: 0 ;
+        left: 0;
         width: 100%;
     }
 }
